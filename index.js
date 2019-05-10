67f3f150c5aaa2ddf586518904c543056fc08b5b
decent-hacker-5309 <jiahaoln@gmail.com>
2019-05-10 20:08:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,6 +24,10 @@ function add2(n) {
   // Feel free to move things around!
   const two = 2
 }
+function add2(n) {
+ var two = 2
+  return n + two;
+}
 
 var funkyFunction = function() {
   return function() {
